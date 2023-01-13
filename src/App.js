eace9ec87eadbab85b87e7d8a8d1305392eb77f4
Ying <ying__li@hotmail.com>
2023-01-13 14:21:26 -0500

    finished app

--- src/App.js
+++ src/App.js
@@ -6,32 +6,37 @@ import Home from './components/Home'
 import About from './components/About'
 import Packages from './components/Packages'
 
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Nav from 'react-bootstrap/Nav'
+import Container from 'react-bootstrap/Container'
 
 function App() {
   
   const packages = ['Activate your Crystals', 'Monkey Meditation', 'Soak in the Hotsprings', 'Hypnotherapy', 'Mineral Bath']
-
+  
 
   return (
     <div className="App">
       <Router>
-        <header>
-          <h1 className="title">Welcome to Monty's Mineral SPA</h1>
-
-          <div className="navBar">
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/about">About Us</Link>
-              </li>
-              <li>
-                <Link to="/packages">Our Packages</Link>
-              </li>
-            </ul>
-          </div>
-        </header>
+      <Container>
+          <Nav defaultActiveKey="/" variant="tabs" fill>
+              <Nav.Item>
+                  <Nav.Link href="/"> 
+                      <Link to="/">Home</Link>
+                  </Nav.Link>
+              </Nav.Item>
+              <Nav.Item >
+                  <Nav.Link eventKey={"aboutPage"}> 
+                      <Link to="/about">About Us</Link>
+                  </Nav.Link>
+              </Nav.Item>
+              <Nav.Item >
+                  <Nav.Link eventKey={"packagesPage"}> 
+                      <Link to="/packages">Our Packages</Link> 
+                  </Nav.Link>
+              </Nav.Item>
+          </Nav>
+      </Container>
 
         <div className="display">
           <Route path="/" component={Home} />
@@ -46,8 +51,3 @@ function App() {
 }
 
 export default App;
-
-
-
-
-
